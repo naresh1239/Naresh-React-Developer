dd7c1c70afea0dc7b40f510d3297a9982105bb26
Naresh kumawat <iamnareshkumawat1@gmail.com>
2022-01-13 20:18:00 +1300

    final

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <BrowserRouter><App /></BrowserRouter>,
+  <App />,
   document.getElementById('root')
 );
 
