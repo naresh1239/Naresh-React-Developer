74b8965e277293ac2bf04d49b654511419a93a1e
Naresh kumawat <iamnareshkumawat1@gmail.com>
2022-01-13 20:16:22 +1300

    finaling i got a idea

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 
 import Home from "./pages/Home"
- import Resume from "./pages/Resume"
-import { Switch, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
    <>
-       <Switch>
-      <Route path="/" exact component={Home}></Route> 
-      <Route path="/newgitreactapp"  component={Resume}></Route>
-      <Home/>
-      </Switch>
+     <Home/>
       </>
     
   );
